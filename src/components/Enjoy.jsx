650f7cb0fee52ebd45a8f7aedf3f05cdc15024be
tekkö <claireholmaert@gmail.com>
2024-05-16 12:51:13 +0200

    👌 IMPROVE: refactor bg

--- src/components/Enjoy.jsx
+++ src/components/Enjoy.jsx
@@ -3,13 +3,12 @@ import Image from '../../public/medias/images/enjoy.jpeg';
 
 function Enjoy() {
   return (
-    <div className='hidden'>
-      <div className='relative'>
-        <img className='absolute w-full z-0' src={Image} alt="" />
-        <div className='absolute top-80 w-full flex items-center justify-center'>
-        <div className='backdrop-blur-sm z-10 w-1/3 flex flex-col items-center justify-center p-20 border-8 border-yellow text-white space-y-5 tracking-wide'>
+    <div className=''>
+      <div className='bg-no-repeat bg-cover w-full h-full p-40' style={{ backgroundImage: `url(${Image})`}}>
+        <div className='top-80 w-full flex items-center justify-center'>
+        <div className='backdrop-blur-sm z-10 w-2/5 flex flex-col items-center justify-center p-20 border-8 border-yellow text-white space-y-5 tracking-wide'>
           <h2 className='text-4xl'>Enjoy Our Food</h2>
-          <p>It's treating yourself to a moment of pure indulgence and conviviality. Our team of talented chefs takes pride in preparing each dish with fresh, local ingredients, ensuring authentic and delicious flavors. Whether for a brunch with friends, a family lunch or a solo gourmet break, our varied menu will satisfy all your desires. Each bite is an invitation to discover exquisite textures and refined tastes, all in a warm and welcoming atmosphere. Come and enjoy our food for an unforgettable dining experience.</p>
+          <p className='text-center'>It's treating yourself to a moment of pure indulgence and conviviality. Our team of talented chefs takes pride in preparing each dish with fresh, local ingredients, ensuring authentic and delicious flavors. Whether for a brunch with friends, a family lunch or a solo gourmet break, our varied menu will satisfy all your desires. Each bite is an invitation to discover exquisite textures and refined tastes, all in a warm and welcoming atmosphere. Come and enjoy our food for an unforgettable dining experience.</p>
         </div>
       </div>
       </div>
