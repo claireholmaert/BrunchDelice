7f94f44c197b26c5f7db4e7399ba0f2367add94e
tekkö <claireholmaert@gmail.com>
2024-05-17 08:58:33 +0200

    👌 IMPROVE: remove container on quality

--- src/components/Quality.jsx
+++ src/components/Quality.jsx
@@ -1,13 +1,9 @@
 // images
 import Image from '../../public/medias/images/plat-fraise.png';
 
-// elements
-import Container from './elements/Container';
-
 function Quality() {
   return (
     <div className="bg-yellow w-full rounded-sm mt-20">
-      <Container>
       <div className="flex items-center p-40 space-x-10">
         <div className="w-full space-y-5 text-white tracking-wide">
             <h2 className="text-4xl">Our Food Quality</h2>
@@ -17,7 +13,6 @@ function Quality() {
             <img src={Image} alt="" />
         </div>
       </div>
-      </Container>
     </div>
   )
 }
